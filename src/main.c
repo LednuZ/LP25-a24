bcded751d8f667543e38807837905abef882f4f6
Pierre Zundel <pierre.zundel@gmail.com>
2024-12-10 10:38:08 +0100

    continuité des options

--- src/main.c
+++ src/main.c
@@ -14,23 +14,43 @@ int main(int argc, char *argv[]) {
     // Exemples : gestion des options --backup, --restore, etc.
     int opt = 0;
     struct option my_opts[] = {
-        {.name="--backup",.has_arg=0,.flag=0,.val='b'},  //à compléter
-        {.name="--restore",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--list-backups",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--dry-run",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--d-server",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--d-port",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--s-server",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--s-port",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--dest",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--source",.has_arg=0,.flag=0,.val=''}, //à compléter
-        {.name="--verbose",.has_arg=0,.flag=0,.val=''} //à compléter
+        {.name="--backup",.has_arg=0,.flag=NULL,.val='b'},  //à compléter
+        {.name="--restore",.has_arg=0,.flag=NULL,.val='r'}, //à compléter
+        {.name="--list-backups",.has_arg=0,.flag=NULL,.val='l'}, //à compléter
+        {.name="--dry-run",.has_arg=0,.flag=NULL,.val='c'}, //à compléter
+        {.name="--d-server",.has_arg=0,.flag=NULL,.val='i'}, //à compléter
+        {.name="--d-port",.has_arg=0,.flag=NULL,.val='j'}, //à compléter
+        {.name="--s-server",.has_arg=0,.flag=NULL,.val='k'}, //à compléter
+        {.name="--s-port",.has_arg=0,.flag=NULL,.val='m'}, //à compléter
+        {.name="--dest",.has_arg=0,.flag=NULL,.val='d'}, //à compléter
+        {.name="--source",.has_arg=0,.flag=NULL,.val='s'}, //à compléter
+        {.name="--verbose",.has_arg=0,.flag=NULL,.val='v'} //à compléter
     }
 
-    while ((opt = getopt(argc, argv, ))!= -1){
+    while ((opt = getopt_long(argc, argv, ))!= -1){
         switch (opt){ //à compléter
-            case 'b': //à compléter
-                break; //à compléter
+            case 'b': // --backup
+                break; 
+            case 'r': // --restore
+                break;
+            case 'l': // --list-backups
+                break;
+            case 'c': // --dry-run
+                break;
+            case 'i': // --d-server
+                break;
+            case 'j': // --d-port
+                break;
+            case 'k': // --s-server
+                break;
+            case 'm': // --s-port
+                break;
+            case 'd': // --dest
+                break;
+            case 's': // --source
+                break;
+            case 'v': // --verbose
+                break;
         } 
     }    
     return EXIT_SUCCESS;
