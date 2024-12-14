67ed9515482b807100a67ce621d553d2c6b0b57b
Pierre Zundel <pierre.zundel@gmail.com>
2024-12-14 18:06:39 +0100

    Bonnes versions de find_md5 et add_md5

--- src/deduplication.c
+++ src/deduplication.c
@@ -10,7 +10,7 @@
 // dans la table de hachage
 unsigned int hash_md5(unsigned char *md5) {
     unsigned int hash = 0;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    for (int i=0; i<MD5_DIGEST_LENGTH; i++) {
         hash = (hash << 5) + hash + md5[i];
     }
     return hash % HASH_TABLE_SIZE;
@@ -31,26 +31,56 @@ int find_md5(Md5Entry *hash_table, unsigned char *md5) {
     *           md5 est le md5 du chunk dont on veut déterminer l'unicité
     *  @return: retourne l'index s'il trouve le md5 dans le tableau et -1 sinon
     */
-    int i=0;
-    while(hash_table){ //tant qu'il a des valeurs dans le tableau de hachage
-        if (strcmp(hash_table[i].md5, *md5)==0) {
-            return hash_table[i].index; //on retourne son index
+    Md5Entry *parcours = hash_table;
+
+    int liste_non_finie = 1;
+    while(liste_non_finie){ //tant qu'il a des valeurs dans le tableau de hachage
+        if (parcours->index == 0) { // hash_table déclaré par calloc, \
+                                    donc si index et md5 == 0 -> fin de la liste
+            int md5_not_null = 0; // Variable mise à 1 si au moins un char de md5 != 0
+            for (int i=0; i<16; ++i) {
+                if (parcours->md5[i] != 0) {
+                    md5_not_null = 1;
+                }
+            }
+            if (!md5_not_null) {
+                liste_non_finie = 0;
+            }
         }
-        ++i;
+
+        if (liste_non_finie) { // On vérifie d'abord qu'on puisse acceder à parcours->md5
+            if (strcmp(parcours->md5, *md5)==0) {
+                return parcours->index; //on retourne son index
+            }
+            ++parcours; // aller à l'adresse suivante
+        }
     }
     return -1;
 }
 
 // Ajouter un MD5 dans la table de hachage
 void add_md5(Md5Entry *hash_table, unsigned char *md5, int index) {
-    int i=0;
-    hash_table->md5 = malloc(16); // Taille d'un MD5
-    if (hash_table[i].md5 == NULL) {
-        fprintf(stderr, "Erreur : échec de l'allocation de mémoire pour le MD5.\n");
-        return;
+    Md5Entry *parcours = hash_table;
+
+    int liste_non_finie = 1;
+    while(liste_non_finie) { // on cherche à trouver la première case libre de la liste
+        if (parcours->index == 0) {
+            int md5_not_null = 0; // Variable mise à 1 si au moins un char de md5 != 0
+            for (int i=0; i<16; ++i) {
+                if (parcours->md5[i] != 0) {
+                    md5_not_null = 1;
+                }
+            }
+            if (!md5_not_null) {
+                liste_non_finie = 0;
+            }
+        }
+        if (liste_non_finie){
+            ++parcours; // aller à l'adresse suivante
+        }
     }
-    memcpy(hash_table[i].md5, md5, 16);
-    hash_table[i].index = index;
+    memcpy(&(parcours->md5), md5, 16);
+    parcours->index = index;
 }
 
 // Fonction pour convertir un fichier non dédupliqué en tableau de chunks
