22cac5272ad19dbc481dc31928151067f98c246d
sacha1403 <sacha.moebel@gmail.com>
2024-12-14 23:48:13 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,56 +7,10 @@
 #include "backup_manager.h"
 #include "network.h"
 
+static int verbose_flag = 0;
+static int dry_run_flag = 0;
 
-<<<<<<< Updated upstream
-    // Implémentation de la logique de sauvegarde et restauration
-    // Exemples : gestion des options --backup, --restore, etc.
-    int opt = 0;
-    struct option my_opts[] = {
-        {.name="--backup",.has_arg=0,.flag=NULL,.val='b'},  //à compléter
-        {.name="--restore",.has_arg=0,.flag=NULL,.val='r'}, //à compléter
-        {.name="--list-backups",.has_arg=0,.flag=NULL,.val='l'}, //à compléter
-        {.name="--dry-run",.has_arg=0,.flag=NULL,.val='c'}, //à compléter
-        {.name="--d-server",.has_arg=0,.flag=NULL,.val='i'}, //à compléter
-        {.name="--d-port",.has_arg=0,.flag=NULL,.val='j'}, //à compléter
-        {.name="--s-server",.has_arg=0,.flag=NULL,.val='k'}, //à compléter
-        {.name="--s-port",.has_arg=0,.flag=NULL,.val='m'}, //à compléter
-        {.name="--dest",.has_arg=0,.flag=NULL,.val='d'}, //à compléter
-        {.name="--source",.has_arg=0,.flag=NULL,.val='s'}, //à compléter
-        {.name="--verbose",.has_arg=0,.flag=NULL,.val='v'} //à compléter
-    }
-
-    while ((opt = getopt_long(argc, argv, ))!= -1){
-        switch (opt){ //à compléter
-            case 'b': // --backup
-                break; 
-            case 'r': // --restore
-                break;
-            case 'l': // --list-backups
-                break;
-            case 'c': // --dry-run
-                break;
-            case 'i': // --d-server
-                break;
-            case 'j': // --d-port
-                break;
-            case 'k': // --s-server
-                break;
-            case 'm': // --s-port
-                break;
-            case 'd': // --dest
-                break;
-            case 's': // --source
-                break;
-            case 'v': // --verbose
-                break;
-        } 
-    }    
-=======
 int main(int argc, char *argv[]) {
-    static int verbose_flag = 0;
-    static int dry_run_flag = 0;
-
     static struct option long_options[] = {
         {"backup", no_argument, NULL, 'b'},
         {"restore", required_argument, NULL, 'r'},
@@ -74,7 +28,7 @@ int main(int argc, char *argv[]) {
     int option_index = 0;
     int opt;
 
-    const char *source_dir = NULL, *backup_dir = NULL, *server_ip = NULL, *dest_dir = NULL;
+    const char *source_dir = NULL, *dest_dir = NULL, *server_ip = NULL;
     int server_port = 0;
 
     while ((opt = getopt_long(argc, argv, "b:r:l", long_options, &option_index)) != -1) {
