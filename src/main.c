25d24b34527e49cd0fe0630f0901b78b7ddad779
sacha1403 <sacha.moebel@gmail.com>
2024-12-15 00:01:04 +0100

    Options modified

--- src/main.c
+++ src/main.c
@@ -9,19 +9,22 @@
 
 static int verbose_flag = 0;
 static int dry_run_flag = 0;
+static int backup_flag = 0;
+static int restore_flag = 0;
+static int list_flag = 0;
 
 int main(int argc, char *argv[]) {
     static struct option long_options[] = {
-        {"backup", no_argument, NULL, 'b'},
-        {"restore", required_argument, NULL, 'r'},
-        {"list-backups", no_argument, NULL, 'l'},
-        {"d-server", required_argument, NULL, 'd'},
-        {"d-port", required_argument, NULL, 'p'},
-        {"s-server", required_argument, NULL, 's'},
-        {"s-port", required_argument, NULL, 't'},
-        {"dest", required_argument, NULL, 'e'},
-        {"source", required_argument, NULL, 'f'},
-        {"verbose", no_argument, &verbose_flag, 1},
+        {"backup", no_argument, &backup_flag, 'b'},
+        {"restore", no_argument, &restore_flag, 'r'},
+        {"list-backups", no_argument, &list_flag, 'l'},
+        {"d-server", required_argument, NULL, 'j'},
+        {"d-port", required_argument, NULL, 'k'},
+        {"s-server", required_argument, NULL, 'm'},
+        {"s-port", required_argument, NULL, 'n'},
+        {"dest", required_argument, NULL, 'd'},
+        {"source", required_argument, NULL, 's'},
+        {"verbose", no_argument, &verbose_flag, 'v'},
         {0, 0, 0, 0}
     };
 
@@ -34,66 +37,49 @@ int main(int argc, char *argv[]) {
     while ((opt = getopt_long(argc, argv, "b:r:l", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'b': // --backup
-                source_dir = optarg;
+                backup_flag = 1;
                 break;
             case 'r': // --restore
-                backup_dir = optarg;
+                restore_flag = 1;
                 break;
             case 'l': // --list-backups
+                list_flag = 1;
                 break;
-            case 'd': // --d-server
+            case 'j': // --d-server
                 server_ip = optarg;
                 break;
-            case 'p': // --d-port
-                server_port = atoi(optarg);
+            case 'k': // --d-port
+                server_port = optarg;
                 break;
-            case 's': // --s-server
+            case 'm': // --s-server
                 server_ip = optarg;
                 break;
-            case 't': // --s-port
-                server_port = atoi(optarg);
+            case 'n': // --s-port
+                server_port = optarg;
                 break;
-            case 'e': // --dest
+            case 'd': // --dest
                 dest_dir = optarg;
                 break;
-            case 'f': // --source
+            case 's': // --source
                 source_dir = optarg;
                 break;
             case 'v': // --verbose
                 verbose_flag = 1;
                 break;
             case '?': // Unknown option
-                fprintf(stderr, "Unknown option. Use --help for usage instructions.\n");
+                fprintf(stderr, "Option non valide.\n");
                 return EXIT_FAILURE;
         }
     }
 
-    if (!source_dir && !backup_dir && !dest_dir) {
-        fprintf(stderr, "Error: Missing required options. Use --help for usage instructions.\n");
+    if (backup_flag + restore_flag + list_flag != 1) {
+        fprintf(stderr, "Error: Vous devez utiliser une seule option parmi : --backup, --restore, --list_backups.\n");
         return EXIT_FAILURE;
     }
 
     if (verbose_flag) {
-        printf("Verbose mode enabled\n");
+        printf("Mode verbose actif\n");
     }
 
-    if (backup_dir) {
-        if (dry_run_flag) {
-            printf("[DRY-RUN] Would restore backup from %s to %s\n", backup_dir, dest_dir);
-        } else {
-            restore(backup_dir, dest_dir, server_ip, server_port, verbose_flag);
-        }
-    } else if (source_dir && dest_dir) {
-        if (dry_run_flag) {
-            printf("[DRY-RUN] Would create backup from %s to %s\n", source_dir, dest_dir);
-        } else {
-            create_backup(source_dir, dest_dir, server_ip, server_port, verbose_flag);
-        }
-    } else {
-        fprintf(stderr, "Error: Invalid combination of options. Use --help for usage instructions.\n");
-        return EXIT_FAILURE;
-    }
-
->>>>>>> Stashed changes
     return EXIT_SUCCESS;
 }
